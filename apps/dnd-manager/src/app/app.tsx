31bd02a31c3eb88a132ef9ddec12976e210e14c8
myin <minyin142p@gmail.com>
2022-07-13 18:23:13 +0200

    add tailwind prettier

--- apps/dnd-manager/src/app/app.tsx
+++ apps/dnd-manager/src/app/app.tsx
@@ -3,7 +3,7 @@ import styles from './app.module.scss';
 
 export function App() {
   return (
-    <div className="bg-black">
+    <div className=" bg-black xl:bg-white">
       <h1 className="text-white">Hello World</h1>
     </div>
   );
