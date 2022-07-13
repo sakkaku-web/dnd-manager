28aba1f76275f6d7dc88f3c19cc4c5bb38056db3
myin <minyin142p@gmail.com>
2022-07-13 18:28:52 +0200

    add single interface for dnd data api

--- libs/dnd-data-api/src/lib/dnd-data-api.ts
+++ libs/dnd-data-api/src/lib/dnd-data-api.ts
@@ -1,3 +1,19 @@
-export function dndDataApi(): string {
-  return 'dnd-data-api';
+export interface DNDDataApi {
+  createSession(id: string): void;
+  addPlayer(player: PlayerData): void;
+}
+
+export interface PlayerData {
+  name: string;
+  class: string;
+  stats: Record<Stats, number>;
+}
+
+export enum Stats {
+  cha,
+  con,
+  dex,
+  int,
+  str,
+  wis,
 }
