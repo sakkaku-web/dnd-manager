a0283356b82964edfc62213c31be253e980e11ea
myin <minyin142p@gmail.com>
2022-07-15 15:37:41 +0200

    add prefix to storage key

--- libs/dnd-data-api/src/lib/local-storage-data-api.ts
+++ libs/dnd-data-api/src/lib/local-storage-data-api.ts
@@ -1,6 +1,8 @@
 import { nanoid } from 'nanoid';
 import { DNDDataApi, PlayerData } from './dnd-data-api';
 
+const STORAGE_KEY_PREFIX = 'dnd-manager-';
+
 interface Data {
   players: PlayerData[];
 }
@@ -9,19 +11,28 @@ export class LocalStorageDataApi implements DNDDataApi {
   createSession(): string {
     const id = nanoid();
     console.log(`Creating session with id ${id}`);
-    localStorage.setItem(id, JSON.stringify({ players: [] }));
+    this.setItem(id, { players: [] });
     return id;
   }
 
   addPlayer(id: string, player: PlayerData): void {
-    const data = localStorage.getItem(id);
+    const data: Data = this.getItem(id);
     if (!data) {
       console.log(`Session with id ${id} does not exist`);
       throw new Error('Session does not exist');
     }
 
-    const json: Data = JSON.parse(data);
-    json.players.push(player);
-    localStorage.setItem(id, JSON.stringify(json));
+    data.players.push(player);
+    this.setItem(id, data);
+  }
+
+  private setItem(key: string, value: object | string) {
+    const str = typeof value === 'object' ? JSON.stringify(value) : value;
+    localStorage.setItem(STORAGE_KEY_PREFIX + key, str);
+  }
+
+  private getItem<T>(key: string): T {
+    const str = localStorage.getItem(STORAGE_KEY_PREFIX + key);
+    return str ? JSON.parse(str) : null;
   }
 }
