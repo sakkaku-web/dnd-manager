afa1991a8c645ec71ca6b4d62dbb177fff41d121
Hao <74541600+haod98@users.noreply.github.com>
2022-07-17 16:06:14 +0200

    Load character stats

--- apps/dnd-manager/src/app/components/CreatePlayer.tsx
+++ apps/dnd-manager/src/app/components/CreatePlayer.tsx
@@ -7,14 +7,24 @@ interface CharacterClass {
   url: string;
 }
 
+interface CharacterStats {
+  name: string;
+}
+
 function CreatePlayer() {
   const [classes, setClasses] = useState<CharacterClass[]>([]);
+  const [stats, setStats] = useState<CharacterStats[]>([]);
   const API_CLASSES = 'https://www.dnd5eapi.co/api/classes';
+  const API_STATS = ' https://www.dnd5eapi.co/api/ability-scores';
 
   useEffect(() => {
     axios.get(API_CLASSES).then((response) => {
       const data = response.data.results;
       setClasses(data);
+    });
+    axios.get(API_STATS).then((response) => {
+      const data = response.data.results;
+      setStats(data);
     });
   }, []);
 
@@ -32,6 +42,14 @@ function CreatePlayer() {
           );
         })}
       </select>
+      {stats.map((characterStats, index) => {
+        return (
+          <div key={index}>
+            <label htmlFor={characterStats.name}>{characterStats.name}</label>
+            <input id={characterStats.name} type="text" />
+          </div>
+        );
+      })}
     </div>
   );
 }
